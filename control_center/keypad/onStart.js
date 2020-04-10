c4c4df8dacc3501957db2aa5354db05496c15900
jeff-hykin <jeff.hykin@gmail.com>
2020-04-09 22:06:08 -0500

    - fix keypad motion sensor mismatch

--- control_center/keypad/onStart.js
+++ control_center/keypad/onStart.js
@@ -2,11 +2,9 @@ let processManager = require("../processManager")
 let setupProcess = require("../commandlineHelper")
 
 // import the official listener-names for this process
-let listeners = processManager.processes.motionSensor.listensFor
-let yell = processManager.processes.motionSensor.canYell
+let listeners = processManager.processes.keypad.listensFor
+let yell = processManager.processes.keypad.canYell
 // start the process
 let theProcess = setupProcess(`python ${__dirname}/process.py`, (response)=>{
-    if (response.trim() == "motionFound") {
-        yell.motionFound()
-    }
+    yell.keyPressed(response.trim())
 })
--- control_center/motionSensor/onStart.js
+++ control_center/motionSensor/onStart.js
@@ -2,9 +2,11 @@ let processManager = require("../processManager")
 let setupProcess = require("../commandlineHelper")
 
 // import the official listener-names for this process
-let listeners = processManager.processes.keypad.listensFor
-let yell = processManager.processes.keypad.canYell
+let listeners = processManager.processes.motionSensor.listensFor
+let yell = processManager.processes.motionSensor.canYell
 // start the process
 let theProcess = setupProcess(`python ${__dirname}/process.py`, (response)=>{
-    yell.keyPressed(response.trim())
+    if (response.trim() == "motionFound") {
+        yell.motionFound()
+    }
 })
