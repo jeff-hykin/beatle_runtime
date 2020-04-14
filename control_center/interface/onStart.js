68d771794b32b0a3ac1603664767bf0ca7a47347
jeff-hykin <jeff.hykin@gmail.com>
2020-04-14 14:56:32 -0500

    -

--- control_center/interface/onStart.js
+++ control_center/interface/onStart.js
@@ -3,8 +3,8 @@ let Interface = require(global.pathFor.systemDataStoragePath)
 let fs = require("fs")
 
 // import the official listener-names for this process
-let listeners = processManager.processes.systemData.listensFor
-let yell = processManager.processes.Interface.canYell
+let listeners = processManager.processes.interface.listensFor
+let yell = processManager.processes.interface.canYell
 
 listeners.attemptLogin = function(newData) {
     console.log(newData)
